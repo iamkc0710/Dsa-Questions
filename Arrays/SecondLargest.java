91d63a9d5639846066788427800597bdf1c7ddcb
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-10-16 21:12:34 +0530

    first

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+
+        int a[] = {1, 7, 6, 4, 5};
+        int largest = a[0];
+        int Slargest = -1;
+
+        for(int i = 1; i < a.length; i++){
+            if(a[i] > largest){
+                Slargest = largest;
+                largest = a[i];
+            }
+            else if(a[i] < largest && a[i] > Slargest){
+                Slargest = a[i];
+            }
+        }
+    
+        System.out.println("Largest Element in the array : " +Slargest);
+  
+    }
+}
