45097ae802bfd726a35a002650610f56b3330039
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-10-16 21:03:17 +0530

    first

---
+++ Arrays/LargestElement.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        int largestNum = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > largestNum){
+                largestNum = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element in the array : " +largestNum);
+    }
+}
