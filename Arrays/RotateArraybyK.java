bd60842b8dcc75dbb25dca156439c9684f43b11f
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-10-20 19:03:44 +0530

    1910

--- Arrays/RotateArraybyK.java
+++ Arrays/RotateArraybyK.java
@@ -1,30 +1,32 @@
 package Arrays;
 
 public class RotateArraybyK {
+
+    public static void reverseNum(int nums[], int start, int end){
+        while(start < end){
+            int swap = nums[start];
+            nums[start] = nums[end];
+            nums[end] = swap;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
         int nums[] = {1,2,3,4,5,6,7};
-        int k = 3;   
-        
-        // Right Rotate
+        int k = 3;
 
-        int temp[] = new int[k+1];
+        int n = nums.length;
+        k %= n;
 
-        for(int i = 0; i < nums.length-k; i++){
-            temp[i] = nums[i];
+        reverseNum(nums,0,n-1);
+        reverseNum(nums,0,k-1);
+        reverseNum(nums,k,n-1);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] +" ");
         }
 
-        for(int i = 0; i < k; i++){
-            nums[i] = nums[nums.length-k+i];
-        }
-
-        for(int i = 0; i < nums.length-k ; i++){
-            nums[k+i] = temp[i];
-        }
-
-
-        for(int i = 0; i < nums.length; i++){
-            System.out.print(nums[i] =" ");
-        }
 
     }
 }
